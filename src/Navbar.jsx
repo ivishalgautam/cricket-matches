6a1e11c6d393e7e5e80db5a42425683d574bb199
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-03-20 12:47:46 +0530

    made it better

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Navbar = () => {
   return (
     <Nav>
-      <div>Logo</div>
+      <Link
+        to="/"
+        style={{ textDecoration: "none", color: "white", fontSize: "1.5rem" }}
+      >
+        <span>Logo</span>
+      </Link>
       <button>Login</button>
     </Nav>
   );
